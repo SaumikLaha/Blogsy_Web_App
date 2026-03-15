b32b789949a5348aeb1ba1e3305359207cf708b9
Saumik Laha <saumiklaha@gmail.com>
2026-03-16 00:14:42 +0530

    updated url

--- backend/jwt/AuthToken.js
+++ backend/jwt/AuthToken.js
@@ -9,9 +9,9 @@ const createTokenAndSaveCookies = async (userId, res) => {
   );
 
   res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: false,        
-    sameSite: "lax",
+    httpOnly: true,  // secure 
+    secure: true,       // required for HTTPS deploy
+    sameSite: "none", // frontend-backend cross domain
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
