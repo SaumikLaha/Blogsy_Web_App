ae91d8cdf9fe2199e7c997b424a1afeb7c316050
Saumik Laha <saumiklaha@gmail.com>
2026-03-16 13:38:29 +0530

    fix update blog api

--- frontend/src/dashboard/UpdateBlog.jsx
+++ frontend/src/dashboard/UpdateBlog.jsx
@@ -33,7 +33,7 @@ function UpdateBlog() {
     const fetchBlog = async () => {
       try {
         const { data } = await axios.get(
-          `http://localhost:4001/api/blogs/single-blog/${id}`,
+           `${import.meta.env.VITE_API_URL}/api/blogs/single-blog/${id}`,
           { withCredentials: true }
         );
 
@@ -65,7 +65,7 @@ function UpdateBlog() {
 
     try {
       const { data } = await axios.put(
-        `http://localhost:4001/api/blogs/update/${id}`,
+       `${import.meta.env.VITE_API_URL}/api/blogs/update/${id}`,
         formData,
         { withCredentials: true }
       );
